964c781fa17a656d78a315d5c590abd1ff47e7db
usama <usamaaslam888@gmail.com>
2023-09-03 23:22:44 +0500

    code clean up

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import { FC } from "react";
+import { Text, StyleSheet } from "react-native";
 import Tabs from "./src/stack";
-import { Text, View } from "react-native";
 
-const App = () => {
+const App: FC = () => {
   return (
     <>
-      <View>
-        <Text style={{fontSize:18,backgroundColor:'white',textAlign:'center',marginTop:12,color:'black'}}>Checkins</Text>
-      </View>
+      <Text style={styles.title}>Checkins</Text>
       <Tabs />
     </>
   )
 }
 export default App
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    backgroundColor: 'white',
+    textAlign: 'center',
+    marginTop: 12,
+    color: 'black'
+  }
+})
